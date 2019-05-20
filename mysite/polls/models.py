c33749411210f21234b74ac9551a2509840d52ec
Davis Raimon <davisraimon@gmail.com>
2019-05-20 11:21:18 +0530

    refs #7007 Template added commit

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,7 +4,8 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    score_int = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('Admission Date')
 
     def __str__(self):
         return self.question_text
